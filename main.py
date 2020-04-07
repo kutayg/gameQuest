f34e5733ce451aca8844ab242e5435be470cff6e
kutayg <62352236+kutayg@users.noreply.github.com>
2020-04-06 21:10:19 -0700

    questAddition
    Add monsters, platforms don't overlap, no limitless "up" key, only jumping

--- main.py
+++ main.py
@@ -8,6 +8,8 @@
 # Modularity, Github, import as, 
 
 import pygame as pg
+from threading import *
+from time import *
 from pygame.sprite import Group
 # from pg.sprite import Group
 import random
@@ -23,29 +25,67 @@ class Game:
         pg.display.set_caption(TITLE)
         self.clock = pg.time.Clock()
         self.running = True
-
     def new(self):
         # start a new game
         self.all_sprites = Group()
-        self.platforms = pg.sprite.Group()
+        self.platforms = Group()
+        self.monsters = Group()
+        self.platcount = 0
+        self.projectiles = Group()
         self.player = Player(self)
         self.all_sprites.add(self.player)
-        ground = Platform(0, HEIGHT-40, WIDTH, 40)
-        plat1 = Platform(200, 400, 150, 20)
-        plat2 = Platform(150, 300, 150, 20)
+        ground = Platform(self, 0, HEIGHT-40, WIDTH, 40)
+        # plat1 = Platform(200, 400, 150, 20)
+        # plat2 = Platform(150, 300, 150, 20)
         self.all_sprites.add(ground)
         self.platforms.add(ground)
-        self.all_sprites.add(plat1)
-        self.platforms.add(plat1)
-        self.all_sprites.add(plat2)
-        self.platforms.add(plat2)
-        for plat in range(1,5):
-            plat = Platform(random.randint(0, WIDTH), random.randint(0, HEIGHT), 200, 20)
-            self.all_sprites.add(plat)
-            self.platforms.add(plat)
+        print(*self.platforms)
+        self.tempGroup = Group()
+        # self.all_sprites.add(plat1)
+        # self.platforms.add(plat1)
+        # self.all_sprites.add(plat2)
+        # self.platforms.add(plat2)
+        # generates platforms that don't touch each other...
+        # cite sources...
+        for plat in range(0,10):
+            if len(self.platforms) < 2:
+                plat = Platform(random.randint(0,WIDTH-100), random.randint(0,HEIGHT-100), 100, 15)
+                self.platforms.add(plat)
+                self.all_sprites.add(plat)
+                # print(self.platforms)
+            # break
+            while True:
+                newPlat = Platform(random.randint(0,WIDTH-100), random.randint(0,HEIGHT-100), 100, 15)
+                self.tempGroup.add(newPlat)
+                selfCollide = pg.sprite.groupcollide(self.tempGroup, self.platforms, True, False)
+                allCollide = pg.sprite.groupcollide(self.tempGroup, self.all_sprites, True, False)
+                if not selfCollide and not allCollide:
+                    self.platforms.add(newPlat)
+                    self.all_sprites.add(newPlat)
+                    self.tempGroup.remove(newPlat)
+                    # print(len(self.tempGroup))
+                    break
+
         self.run()
-
-
+    def platGen(self):
+        for plat in range(0, 15):
+            if len(self.platforms) < 2:
+                plat = Platform(self, random.randint(0,WIDTH-100), random.randint(0,HEIGHT-100), 100, 15)
+                self.platforms.add(plat)
+                self.all_sprites.add(plat)
+                # print(self.platforms)
+            # break
+            while True:
+                newPlat = Platform(self, random.randint(0,WIDTH-100), random.randint(0,HEIGHT-100), 100, 15)
+                self.tempGroup.add(newPlat)
+                selfCollide = pg.sprite.groupcollide(self.tempGroup, self.platforms, True, False)
+                allCollide = pg.sprite.groupcollide(self.tempGroup, self.all_sprites, True, False)
+                if not selfCollide and not allCollide:
+                    self.platforms.add(newPlat)
+                    self.all_sprites.add(newPlat)
+                    self.tempGroup.remove(newPlat)
+                    # print(len(self.tempGroup))
+                    break
     def run(self):
         # Game Loop
         self.playing = True
@@ -54,17 +94,36 @@ class Game:
             self.events()
             self.update()
             self.draw()
-
     def update(self):
-        # Game Loop - Update
+        # Update - listen to see if anything changes...
         self.all_sprites.update()
+        for p in self.projectiles:
+            # print(p.birth)
+            if p.rect.y < 0:
+                p.kill()
+                # print(self.projectiles)
+        phits = pg.sprite.groupcollide(self.projectiles, self.platforms, True, True)
+        if phits:
+            pass
+            # print("a projectile collided with a plat...")
         hits = pg.sprite.spritecollide(self.player, self.platforms, False)
         if hits:
-            # print("it collided")
-            self.player.vel.y = 0
-            self.player.pos.y = hits[0].rect.top+1
-            
-
+            if self.player.rect.top > hits[0].rect.top:
+                print("i hit my head")
+                self.player.vel.y = 15
+                self.player.rect.top = hits[0].rect.bottom + 5
+            else:
+                self.player.vel.y = 0
+                self.player.pos.y = hits[0].rect.top+1
+        if self.player.rect.top <= HEIGHT / 4:
+            self.player.pos.y += abs(self.player.vel.y)
+            for plat in self.platforms:
+                plat.rect.y += abs(self.player.vel.y)
+                if plat.rect.top >= HEIGHT:
+                    plat.kill()
+                    print(len(self.platforms))
+        if len(self.platforms) < 10:
+            self.platGen()  
     def events(self):
         # Game Loop - events
         for event in pg.event.get():
@@ -73,18 +132,15 @@ class Game:
                 if self.playing:
                     self.playing = False
                 self.running = False
-
     def draw(self):
         # Game Loop - draw
-        self.screen.fill(BLACK)
+        self.screen.fill(AQUA)
         self.all_sprites.draw(self.screen)
         # *after* drawing everything, flip the display
         pg.display.flip()
-
     def show_start_screen(self):
         # game splash/start screen
         pass
-
     def show_go_screen(self):
         # game over/continue
         pass
--- sprites.py
+++ sprites.py
@@ -2,6 +2,8 @@
 # © 2019 KidsCanCode LLC / All rights reserved.
 # mr cozort planted a landmine by importing Sprite directly...
 import pygame as pg
+from threading import *
+import time
 from pygame.sprite import Sprite
 from settings import *
 vec = pg.math.Vector2
@@ -12,35 +14,44 @@ class Player(Sprite):
         Sprite.__init__(self)
         self.game = game
         self.image = pg.Surface((30, 40))
-        self.image.fill(YELLOW)
+        self.image.fill(PURPLE)
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH / 2, HEIGHT / 2)
         self.pos = vec(WIDTH / 2, HEIGHT / 2)
         self.vel = vec(0, 0)
         self.acc = vec(0, 0)
-    def myMethod(self):
-        pass
+        self.hitpoints = 100
+    def pew(self):
+        lazer = Pewpew(self.game, self.pos.x + self.rect.width/2, self.pos.y, 10, 10)
+        # print("trying to pewpewpew")
+        self.game.all_sprites.add(lazer)
+        self.game.platforms.add(lazer)
+        self.game.projectiles.add(lazer)
     def jump(self):
         self.rect.x += 1
         hits = pg.sprite.spritecollide(self, self.game.platforms, False)
         self.rect.x -= 1
         if hits: 
-            self.vel.y = -20
+            self.vel.y = -15
     def update(self):
         self.acc = vec(0, 0.5)
         keys = pg.key.get_pressed()
         if keys[pg.K_a]:
             self.acc.x = -PLAYER_ACC
+            print(self.acc.x)
+            print(self.acc)
+            print(self.acc.x)
         if keys[pg.K_d]:
             self.acc.x = PLAYER_ACC
         if keys[pg.K_w]:
-            self.acc.y = -PLAYER_ACC
+            pass
+            # self.acc.y = -PLAYER_ACC
         if keys[pg.K_s]:
             self.acc.y = PLAYER_ACC
+            # self.acc.y = PLAYER_ACC
         # ALERT - Mr. Cozort did this WAY differently than Mr. Bradfield...
         if keys[pg.K_SPACE]:
             self.jump()
-
         # apply friction
         self.acc.x += self.vel.x * PLAYER_FRICTION
         # self.acc.y += self.vel.y * PLAYER_FRICTION
@@ -56,13 +67,74 @@ class Player(Sprite):
             self.pos.y = HEIGHT
         if self.pos.y > HEIGHT:
             self.pos.y = 0
-
         self.rect.midbottom = self.pos
+
+class Monster(Sprite):
+    # include game parameter to pass game class as argument in main...
+    def __init__(self, game):
+        Sprite.__init__(self)
+        self.game = game
+        self.image = pg.Surface((30, 40))
+        self.image.fill(LIGHTGREEN)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+        self.pos = vec(WIDTH / 2, HEIGHT / 2)
+        self.vel = vec(0, 0)
+        self.acc = vec(0.5, 0)
+        self.hitpoints = 100        
+        self.rect.midbottom = self.pos
+    def update(self):
+        pass
+        # self.acc = vec(0.5, 0)
+        # apply friction
+        # self.acc.x += self.vel.x * MONSTER_FRICTION
+        # # self.acc.y += self.vel.y * PLAYER_FRICTION
+        # # equations of motion
+        # self.vel += self.acc
+        # self.pos += self.vel + 0.5 * self.acc
+        # # wrap around the sides of the screen
+        # # self.vel.x += 5
+        # self.rect.midbottom = self.pos 
+
 class Platform(Sprite):
     def __init__(self, x, y, w, h):
         Sprite.__init__(self)
+        self.game = game
         self.image = pg.Surface((w, h))
-        self.image.fill(GREEN)
+        self.image.fill(WHITE)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.mob = Monster(self.game)
+        self.mob.pos = self.rect.midtop
+        self.spawn()
+    def spawn(self):
+        self.game.all_sprites.add(self.mob)
+        self.game.monsters.add(self.mob)
+    def update(self):
+        if self.mob.rect.x > self.rect.width:
+            self.mob.acc.x = -0.1
+        if self.mob.rect.x < self.rect.x:
+            self.mob.acc.x = 0.1
+        self.mob.vel += self.mob.acc
+        self.mob.pos += self.mob.vel + 0.5 * self.mob.acc
+        self.mob.acc = vec(0,0)
+        self.mob.rect.midbottom = self.mob.pos
+
+class Pewpew(Sprite):
+    def __init__(self, game, x, y, w, h):
+        Sprite.__init__(self)
+        self.game = game
+        self.image = pg.Surface((w, h))
+        self.image.fill(LIGHTBLUE)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.birth = time.perf_counter_ns()
+        self.lifespan = 3000000000
+
+    def update(self):
+        self.rect.y += 5
+        self.now = time.perf_counter_ns()
+        if self.now - self.birth > self.lifespan:
+            self.kill()
